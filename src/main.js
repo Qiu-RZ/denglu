b0e3e33c708d008f063ba1b93dae8ca50aacf9ba
Qiu <qiu@121.com>
2019-11-19 23:41:55 +0800

    整合路由

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+//导入路由
+import router from './router/router.js'
+
+
 new Vue({
   render: h => h(App),
+  //挂载vue实例
+  router
 }).$mount('#app')
---
+++ src/router/router.js
@@ -0,0 +1,19 @@
+//导入Vue-router
+import VueRouter from 'vue-router'
+
+//导入vue
+import Vue from 'vue'
+
+//使用
+Vue.use(VueRouter)
+
+//路由规则
+const routes = [];
+
+//创建路由实例
+const router = new VueRouter({
+    routes
+});
+
+//暴露出去
+export default router
