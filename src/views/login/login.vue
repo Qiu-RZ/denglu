7b0eab36b5a6f6f7bea424da603f48c7c7813cca
Qiu <qiu@121.com>
2019-11-21 11:14:35 +0800

    登录模块-表单完成

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -9,26 +9,42 @@
         <span class="uesr-login">用户登录</span>
       </div>
       <!-- 输入框 -->
-      <el-input
-        placeholder="请输入手机号"
-        prefix-icon="el-icon-user"
-        class="phone-input"
-        v-model="input2"
-      ></el-input>
-      <el-input
-        placeholder="请输入密码"
-        prefix-icon="el-icon-lock"
-        class="password-input"
-        v-model="input2"
-      ></el-input>
-      <el-row class="captcha-row">
-        <el-col :span="17">
-          <el-input placeholder="请输入验证码" prefix-icon="el-icon-key" v-model="input2"></el-input>
-        </el-col>
-        <el-col :span="7">
-          <img class="captcha" src="../../assets/captcha.png" alt />
-        </el-col>
-      </el-row>
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        status-icon
+        class="demo-ruleForm"
+      >
+        <el-form-item label prop="phone">
+          <el-input
+           v-model="ruleForm.phone"
+            placeholder="请输入手机号"
+            prefix-icon="el-icon-user"
+            class="phone-input"
+          ></el-input>
+        </el-form-item>
+
+     <el-form-item label prop="passwrd">
+        <el-input
+         v-model="ruleForm.passwrd"
+          placeholder="请输入密码"
+          prefix-icon="el-icon-lock"
+          class="password-input"
+        ></el-input>
+           </el-form-item>
+            <el-form-item label prop="captcha">
+        <el-row class="captcha-row">
+          <el-col :span="17">
+            <el-input placeholder="请输入验证码" prefix-icon="el-icon-key"  v-model="ruleForm.captcha"></el-input>
+          </el-col>
+          <el-col :span="7">
+            <img class="captcha" src="../../assets/captcha.png" alt />
+          </el-col>
+        </el-row>
+        </el-form-item>
+
+      </el-form>
 
       <!-- 协议 -->
       <el-checkbox class="checkbox">
@@ -48,7 +64,49 @@
 
 <script>
 export default {
-  name: "login"
+  name: "login",
+
+  data() {
+    //自定义规则
+    const checkAge = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error("手机号不能为空"));
+      } else {
+        const res = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/;
+        if (res.test(value)) {
+          callback();
+        } else {
+          callback(new Error("手机号格式不对哦"));
+        }
+      }
+    };
+    return {
+      ruleForm: {
+        //手机
+        phone: "",
+        //密码
+        passwrd: "",
+        //验证码
+        captcha: ""
+      },
+      rules: {
+        //手机号
+        phone: [{ validator: checkAge, required: true }],
+        //密码
+        passwrd: [
+          { required: true, message: "密码不能为空" },
+          { min: 4, max: 8, message: "长度在 4 到 8 个字符" }
+        ],
+        //验证码:
+        captcha: [
+          { required: true, message: "验证码不能为空" },
+          { min: 4, max: 4, message: "4个字符噢" }
+        ]
+      }
+    };
+  },
+
+  methods: {}
 };
 </script>
 
