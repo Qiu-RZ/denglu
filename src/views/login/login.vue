0a1e4ce712889c37c6ff6c91d55a00e49efa33ea
Qiu <qiu@121.com>
2019-11-21 21:53:29 +0800

    登录模块-整体完成

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -9,51 +9,44 @@
         <span class="uesr-login">用户登录</span>
       </div>
       <!-- 输入框 -->
-      <el-form
-        :model="ruleForm"
-        :rules="rules"
-        ref="ruleForm"
-        status-icon
-        class="demo-ruleForm"
-      >
-        <el-form-item label prop="phone">
+      <el-form :model="loginForm" :rules="loginRules" ref="loginForm" status-icon class="demo-ruleForm">
+        <el-form-item label= "" prop="phone">
           <el-input
-           v-model="ruleForm.phone"
+             v-model="loginForm.phone"
             placeholder="请输入手机号"
             prefix-icon="el-icon-user"
             class="phone-input"
           ></el-input>
         </el-form-item>
 
-     <el-form-item label prop="passwrd">
-        <el-input
-         v-model="ruleForm.passwrd"
-          placeholder="请输入密码"
-          prefix-icon="el-icon-lock"
-          class="password-input"
-        ></el-input>
-           </el-form-item>
-            <el-form-item label prop="captcha">
-        <el-row class="captcha-row">
-          <el-col :span="17">
-            <el-input placeholder="请输入验证码" prefix-icon="el-icon-key"  v-model="ruleForm.captcha"></el-input>
-          </el-col>
-          <el-col :span="7">
-            <img class="captcha" src="../../assets/captcha.png" alt />
-          </el-col>
-        </el-row>
+        <el-form-item label='' prop="passwrd">
+          <el-input
+            v-model="loginForm.passwrd"
+            placeholder="请输入密码"
+            prefix-icon="el-icon-lock"
+            class="password-input"
+          ></el-input>
         </el-form-item>
-
+        <el-form-item label='' prop="captcha">
+          <el-row class="captcha-row">
+            <el-col :span="17">
+              <el-input placeholder="请输入验证码" prefix-icon="el-icon-key" v-model="loginForm.captcha"></el-input>
+            </el-col>
+            <el-col :span="7">
+              <img @click="changeCaptcha" class="captcha" :src="captchaURL" alt />
+            </el-col>
+          </el-row>
+        </el-form-item>
       </el-form>
 
       <!-- 协议 -->
-      <el-checkbox class="checkbox">
+      <el-checkbox class="checkbox" v-model="checked">
         我已阅读并同意
         <el-link type="primary">用户协议</el-link>和
         <el-link type="primary">隐私条款</el-link>
       </el-checkbox>
       <!-- 登录 注册按钮 -->
-      <el-button class="login-btn" type="primary">登录</el-button>
+      <el-button class="login-btn" type="primary"  @click="submitForm('loginForm')">登录</el-button>
       <el-button class="reg-btn" type="primary">注册</el-button>
     </div>
 
@@ -63,6 +56,8 @@
 </template>
 
 <script>
+//导入axios
+import axios from "axios";
 export default {
   name: "login",
 
@@ -81,7 +76,7 @@ export default {
       }
     };
     return {
-      ruleForm: {
+      loginForm: {
         //手机
         phone: "",
         //密码
@@ -89,7 +84,8 @@ export default {
         //验证码
         captcha: ""
       },
-      rules: {
+      //登录验证规则
+      loginRules: {
         //手机号
         phone: [{ validator: checkAge, required: true }],
         //密码
@@ -102,11 +98,59 @@ export default {
           { required: true, message: "验证码不能为空" },
           { min: 4, max: 4, message: "4个字符噢" }
         ]
-      }
+      },
+      //获取验证码地址
+      captchaURL: "http://183.237.67.218:3002/captcha?type=login",
+      //复选框按钮
+      checked: true
     };
   },
 
-  methods: {}
+  methods: {
+    //点击获取验证码
+    changeCaptcha() {
+      this.captchaURL = `http://183.237.67.218:3002/captcha?type=login&${Date.now()}`;
+    },
+    //点击登录按钮:
+    submitForm(formName) {
+      if (!this.checked) {
+        this.$message.warning("协议还没勾选哦,必须要勾选");
+        return;
+      }
+      // this.$refs['ruleForm']==> 获取饿了么的表单
+      // 饿了么的表单.validate()
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          // 验证成功
+          // alert("submit!");
+          // 接口调用
+          axios({
+            url: "http://183.237.67.218:3002/login",
+            method: "post",
+            data: {
+              phone: this.loginForm.phone,
+              password: this.loginForm.password,
+              code: this.loginForm.captcha
+            },
+            withCredentials: true
+          }).then(res => {
+            window.console.log(res);
+            // if (res.data.code == 200) {
+            //   // 成功
+            //   this.$message.success("你可算回来啦！");
+            // } else {
+            //   // 失败
+            //   this.$message.warning("登录失败了哦");
+            // }
+          });
+        } else {
+          // 验证失败
+          window.console.log("error submit!!");
+          return false;
+        }
+      });
+    }
+  }
 };
 </script>
 
